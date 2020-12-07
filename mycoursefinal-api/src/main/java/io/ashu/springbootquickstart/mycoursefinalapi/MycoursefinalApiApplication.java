240a984c93ba7329bf46d5e72815abba795a9036
kumaashu <kumaashu@KUMAASHU02.corp.amdocs.com>
2020-12-07 18:26:26 +0530

    First Commit Uploading Course app

---
+++ mycoursefinal-api/src/main/java/io/ashu/springbootquickstart/mycoursefinalapi/MycoursefinalApiApplication.java
@@ -0,0 +1,13 @@
+package io.ashu.springbootquickstart.mycoursefinalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoursefinalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycoursefinalApiApplication.class, args);
+	}
+
+}
---
+++ mycoursefinal-api/src/test/java/io/ashu/springbootquickstart/mycoursefinalapi/MycoursefinalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ashu.springbootquickstart.mycoursefinalapi;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MycoursefinalApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
